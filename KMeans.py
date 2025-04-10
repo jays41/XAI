b82418babe4ae16c5133bec00844d9413a8fae87
jays41 <jayrs2016@gmail.com>
2025-04-10 23:51:24 +0100

    Add methods for SSE and silhouette score calculations for KMeans

--- KMeans.py
+++ KMeans.py
@@ -40,6 +40,48 @@ class KMeans():
       else:
         self.centroids[i] = np.random.rand(self.num_features)
 
+  def get_sse(self):
+    sse = 0
+    for i in range(self.num_clusters):
+      cluster_data = self.data[self.clusters == i]
+      if len(cluster_data) > 0:
+        for point in cluster_data.values:
+          sse += self.calculate_distance(point, self.centroids[i]) ** 2
+    return sse
+
+  def get_cohesion(self, i):
+    # Cohesion: The average distance between a point and all other points in the same cluster.
+    values = self.data.iloc[i].values
+    cluster_data = self.data[self.clusters == self.clusters[i]]
+    total = 0
+    for point in cluster_data.values:
+      total += self.calculate_distance(point, values)
+    return total / len(cluster_data)
+
+  def get_separation(self, i):
+    # Separation: The average distance between a point and all points in the nearest different cluster.
+    values = self.data.iloc[i].values
+    distances = []
+    for j in range(self.num_clusters):
+      if j == self.clusters[i]:
+        continue
+      distances.append(self.calculate_distance(values, self.centroids[j]))
+    cluster_index = distances.index(min(distances))
+
+    cluster_data = self.data[self.clusters == cluster_index]
+    total = 0
+    for point in cluster_data.values:
+      total += self.calculate_distance(point, values)
+    return total / len(cluster_data)
+
+  def get_silhouette_score(self):
+    score = 0
+    for i in range(len(self.data)):
+      cohesion = self.get_cohesion(i)
+      separation = self.get_separation(i)
+      score += (separation - cohesion) / max(cohesion, separation)
+    return score / len(self.data)
+
   def run(self):
     self.initialise()
     prev_centroids = self.centroids
