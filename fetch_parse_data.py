b340c13a10a92f763e43d524a47e775ac5b503d3
jays41 <jayrs2016@gmail.com>
2025-05-28 18:18:27 +0100

    Enhance data fetching in fetch_parse_data: add error handling for yfinance download and fallback to CSV

--- fetch_parse_data.py
+++ fetch_parse_data.py
@@ -1,14 +1,24 @@
 import pandas as pd
 import numpy as np
 import yfinance as yf
+import time
 
 from indicators import *
 
 
 def fetch_parse_data(startDate="2014-11-01", endDate="2019-01-01"):
     labels = ['RSI', 'MACD', 'Volatility', 'Volume', 'Return', 'Momentum']
-
-    df = yf.download("SPY", start=startDate, end=endDate, group_by='column')
+    try:
+        df = yf.download("SPY", start=startDate, end=endDate, group_by='column')
+    
+    except:
+        print("Failed to download data from yfinance")
+        df = pd.read_csv("data.csv")
+    
+    
+    if df.empty:
+        df = pd.read_csv("data.csv")
+    
     df.columns = df.columns.get_level_values(0)
 
     df['RSI'] = calculate_rsi(df)
